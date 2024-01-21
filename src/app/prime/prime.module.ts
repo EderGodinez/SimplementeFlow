93c97354c474d7d4afe80aaba7400cd2ff1b4acd
Eder Lap <eder.godinez26@gmail.com>
2024-01-20 20:18:00 -0600

    fix:Eliminacion de modulos que no se usaron de primeng

--- src/app/prime/prime.module.ts
+++ src/app/prime/prime.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordModule } from 'primeng/password';
-import { CheckboxModule } from 'primeng/checkbox';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -16,18 +15,14 @@ import { CarouselModule } from 'primeng/carousel';
 import { TableModule } from 'primeng/table';
 import { ChartModule } from 'primeng/chart';
 import { MenuModule } from 'primeng/menu';
-import { StyleClassModule } from 'primeng/styleclass';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { FileUploadModule } from 'primeng/fileupload';
-import { RippleModule } from 'primeng/ripple';
 import { ToolbarModule } from 'primeng/toolbar';
-import { RatingModule } from 'primeng/rating';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { DropdownModule } from 'primeng/dropdown';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { DialogModule } from 'primeng/dialog';
-import { TabViewModule } from 'primeng/tabview';
 import { GalleriaModule } from 'primeng/galleria';
 import { InputMaskModule } from 'primeng/inputmask';
 import { CardModule } from 'primeng/card';
@@ -42,7 +37,6 @@ import { PaginatorModule } from 'primeng/paginator';
   ],
   exports:[
     PasswordModule,
-    CheckboxModule,
     SidebarModule,
     ButtonModule,
     InputTextModule,
@@ -57,25 +51,21 @@ import { PaginatorModule } from 'primeng/paginator';
     TableModule,
     ChartModule,
     MenuModule,
-    StyleClassModule,
     PanelMenuModule,
     FileUploadModule,
-    RippleModule,
     InputTextareaModule,
     ToolbarModule,
-    RatingModule,
     DialogModule,
     DropdownModule,
     RadioButtonModule,
     InputNumberModule,
-    TabViewModule,
     GalleriaModule,
     InputMaskModule,
     CardModule,
     BadgeModule,
     PanelModule,
     SkeletonModule,
-  PaginatorModule
+    PaginatorModule
   ]
 })
 export class PrimeModule { }
